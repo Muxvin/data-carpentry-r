ddae62178b98c711b5937c3033739d8fa037ef20
Mohsen Bafandehkar <mohsen.bafandehkar@students.mq.edu.au>
2019-07-25 12:22:13 +1000

    Data dates downloaded.

--- scripts/Data_Downloads.R
+++ scripts/Data_Downloads.R
@@ -1,2 +1,4 @@
 download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_messy.xlsx",
               destfile = "./data/SAFI_messy.xlsx", mode = "wb")
+download.file(url = "https://mq-software-carpentry.github.io/R-git-for-research/data/SAFI_clean.xlsx",
+              destfile = "./data/SAFI_clean.xlsx", mode = "wb")
