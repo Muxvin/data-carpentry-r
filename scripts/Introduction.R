7331aecf35afe656eea6b515c4b73a074893105d
Mohsen Bafandehkar <mohsen.bafandehkar@students.mq.edu.au>
2019-07-25 16:09:17 +1000

    Data Cohision in R

---
+++ scripts/Introduction.R
@@ -0,0 +1,24 @@
+# load required packages
+# install.packages("tidyverse")
+library(tidyverse)
+print("Hello, world")
+
+area_hectares <- 1.0
+
+width <- 2
+length <- 5
+area <- width*length
+area
+
+round(digits = 2, x = 3.14153)
+no_members <- c(3, 7, 10,  6)
+length(no_members)
+class(no_members)
+str(no_members)
+
+respondent_wall_type <- c("muddam", "dhais", "ketab")
+length(respondent_wall_type)
+class(respondent_wall_type)
+str(respondent_wall_type)
+
+
